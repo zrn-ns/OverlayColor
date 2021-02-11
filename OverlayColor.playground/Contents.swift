5a505f3aba0ef2f63805a5ce66f908e412b50b8c
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2021-02-11 09:56:18 +0900

    ADD 色を重ねるメソッドを定義

--- OverlayColor.playground/Contents.swift
+++ OverlayColor.playground/Contents.swift
@@ -10,6 +10,27 @@ extension UIView {
     }
 }
 
+extension UIColor {
+    /// 色を重ね合わせたときの色を生成する
+    /// 参考 https://ja.wikipedia.org/wiki/アルファブレンド
+    func overlayColor(_ otherColor: UIColor) -> UIColor {
+        var (r1, g1, b1, a1) = (CGFloat(0), CGFloat(0), CGFloat(0), CGFloat(0))
+        var (r2, g2, b2, a2) = (CGFloat(0), CGFloat(0), CGFloat(0), CGFloat(0))
+
+        getRed(&r1, green: &g1, blue: &b1, alpha: &a1)
+        otherColor.getRed(&r2, green: &g2, blue: &b2, alpha: &a2)
+
+        let a3 = a1 + a2 * (1 - a1)
+        let r3 = (r2 * a2 + r1 * a1 * (1 - a2)) / a3
+        let g3 = (g2 * a2 + g1 * a1 * (1 - a2)) / a3
+        let b3 = (b2 * a2 + b1 * a1 * (1 - a2)) / a3
+        return UIColor(red: r3,
+                       green: g3,
+                       blue: b3,
+                       alpha: a3)
+    }
+}
+
 class MyViewController: UIViewController {
     override func loadView() {
         let view = UIView()
